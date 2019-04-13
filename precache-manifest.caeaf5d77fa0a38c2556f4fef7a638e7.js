d53cb3d765429b6331ef75817568fd94336149a4
Kumar Nitesh <contact@kumar-nitesh.com>
2019-04-13 00:36:44 -0400

    Initial Commit

---
+++ precache-manifest.caeaf5d77fa0a38c2556f4fef7a638e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "83e63b511146a75c9c60",
+    "url": "/static/js/main.305ee7f2.chunk.js"
+  },
+  {
+    "revision": "583db8236a561edd09bf",
+    "url": "/static/js/2.e7d830b4.chunk.js"
+  },
+  {
+    "revision": "83e63b511146a75c9c60",
+    "url": "/static/css/main.f20926b8.chunk.css"
+  },
+  {
+    "revision": "583db8236a561edd09bf",
+    "url": "/static/css/2.a24c2de1.chunk.css"
+  },
+  {
+    "revision": "1b7b8828c681d61d700526529a606939",
+    "url": "/index.html"
+  }
+];
